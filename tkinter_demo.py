3fcbc0b822e97c19a8816e8e89b7e8ded971b65d
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2017-05-05 12:06:41 +0200

    Update GUI

--- tkinter_demo.py
+++ tkinter_demo.py
@@ -3,9 +3,6 @@
 import tkinter
 #import game
 
-def getCoordinates(x):
-    print(x.widget.find_closest(x.x, x.y))
-
 class Application(tkinter.Frame):
     def __init__(self, parent):
         tkinter.Frame.__init__(self, parent)
@@ -13,21 +10,43 @@ class Application(tkinter.Frame):
         self.initUI()
 
     def initUI(self):
+        # constants
+        BOARD_SIZE = 350
+        TILE_COUNT = 10
+        TILE_BORDER = 1
+        TILE_SIZE = (BOARD_SIZE / TILE_COUNT) - (TILE_BORDER / TILE_COUNT)
+
+        # functions
+        def click_tile(event):
+            print(event.widget.find_closest(event.x, event.y)[0])
+
+        self.parent.resizable(False, False)
         self.parent.title('Battleships Game')
 
         label = tkinter.Label(self.parent, text='Start new game', font=('Helvetica', 20), pady=10)
         label.pack()
 
-        c1 = tkinter.Canvas(self.parent, width=350, height=350, highlightthickness=0)
+        c1 = tkinter.Canvas(self.parent, width=BOARD_SIZE, height=BOARD_SIZE, highlightthickness=0)
 
-        for x in range(10):
-            for y in range(10):
-                field = c1.create_rectangle(35 * x, 35 * y, 35 * x + 35, 35 * y + 35, width=1, fill='#ddd')
-                c1.tag_bind(field, '<ButtonPress-1>', getCoordinates)
+        for x in range(TILE_COUNT):
+            for y in range(TILE_COUNT):
+                field = c1.create_rectangle(TILE_SIZE * x, TILE_SIZE * y, (TILE_SIZE * x) + TILE_SIZE, (TILE_SIZE * y) + TILE_SIZE, width=1, fill='#ddd', activefill='#eee')
+                c1.tag_bind(field, '<ButtonPress-1>', click_tile)
 
-        c2 = tkinter.Canvas(self.parent, width=350, height=350, highlightthickness=0)
-        c1.pack(side='left', fill='both', expand=True)
-        c2.pack(side='right', fill='both', expand=True)
+        c2 = tkinter.Canvas(self.parent, width=BOARD_SIZE, height=BOARD_SIZE, highlightthickness=0)
+
+        for x in range(TILE_COUNT):
+            for y in range(TILE_COUNT):
+                field = c2.create_rectangle(TILE_SIZE * x, TILE_SIZE * y, (TILE_SIZE * x) + TILE_SIZE, (TILE_SIZE * y) + TILE_SIZE, width=1, fill='#ddd', activefill='#eee')
+                c2.tag_bind(field, '<ButtonPress-1>', click_tile)
+
+        label = tkinter.Label(self.parent, text='Start new game', font=('Helvetica', 20), pady=10)
+        label.pack(side='bottom')
+
+        c1.pack(side='left', padx=(10, 5))
+        c2.pack(side='right', padx=(5, 10))
+
+
 
         #for row in range(10):
             #for column in range(10):
