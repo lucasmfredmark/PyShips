236f31d7b57bbb9fcecea653082f0ff9fcc5cde7
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2017-05-18 18:50:35 +0200

    Improved random shooter

--- classes/ai/RandomShooter.py
+++ classes/ai/RandomShooter.py
@@ -4,10 +4,16 @@ import random
 class RandomShooter(BattleshipsAI):
     def __init__(self):
         BattleshipsAI.__init__(self)
+        self.shots = [[False for y in range(self.BOARD_SIZE)] for x in range(self.BOARD_SIZE)]
 
     def get_shot_position(self, ships):
-        position_x = random.randint(0, self.BOARD_SIZE - 1)
-        position_y = random.randint(0, self.BOARD_SIZE - 1)
+        while True:
+            position_x = random.randint(0, self.BOARD_SIZE - 1)
+            position_y = random.randint(0, self.BOARD_SIZE - 1)
+
+            if not self.shots[position_x][position_y]:
+                self.shots[position_x][position_y] = True
+                break
 
         return position_x, position_y
 
